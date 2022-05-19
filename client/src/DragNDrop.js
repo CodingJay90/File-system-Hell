56253f423962a802365dd9ef9cd1d327357e578b
Faruq <CodingJay90>
2022-05-19 20:26:17 +0100

    folder and file moving feature implemented

--- client/src/DragNDrop.js
+++ client/src/DragNDrop.js
@@ -10,9 +10,8 @@ class DnD {
   constructor() {
     this.trashZone = null;
     this.selectedId = null;
+    this.dropZoneId = null;
     this.type = "";
-    // this.drag = this.drag.bind(this);
-    // this.dropInTrash = this.dropInTrash.bind(this);
     const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
     methods
       .filter((method) => method !== "constructor")
@@ -29,26 +28,50 @@ class DnD {
     this.trashZone.classList.add("delete__zone--over");
     this.selectedId =
       e.currentTarget.dataset.folder_id || e.currentTarget.dataset.file_id;
-    console.log(e.currentTarget.dataset);
   }
 
-  dragLeave() {
-    // console.log("Event: ", "dragleave");
+  dragLeave(e) {
+    e.stopPropagation();
+    // e.currentTarget.classList.remove("explorer__content-folder--over");
   }
 
   dragOver(e) {
-    // console.log("Event: ", "dragover");
     e.preventDefault();
+    e.stopPropagation();
+    // console.log("Event: ", "dragover");
+    // e.currentTarget.classList.add("explorer__content-folder--over");
   }
 
   dragDrop(e) {
     e.preventDefault();
+    e.stopPropagation();
+    this.dropZoneId =
+      e.currentTarget.dataset.folder_id || e.currentTarget.dataset.file_id;
+    // console.log("dropped on", this.dropZoneId);
+    if (this.dropZoneId === this.selectedId) return; //prevent further execution if dragged item id in the same folder as the drop zone
+    this.swap();
   }
 
   dragEnd(e) {
     e.preventDefault();
     e.stopPropagation();
     this.trashZone.classList.remove("delete__zone--over");
+  }
+
+  swap() {
+    let from = selectDomElement(`[id='${this.selectedId}']`);
+    let to = selectDomElement(`[id='${this.dropZoneId}']`);
+    console.log("from ", from);
+    console.log("to ", to);
+    if (from.contains(to)) return; //prevent parent folder getting moved into subfolder
+    if (!to.classList.contains("explorer__content-folder--collapsed"))
+      from.classList.add("d-none"); //add a display none style to moved element if parent element is collapsed
+    if (to.classList.contains("explorer__content-file")) {
+      from.classList.remove("d-none");
+      to.parentElement.appendChild(from);
+      return;
+    }
+    to.appendChild(from);
   }
 
   //trashZone
@@ -61,7 +84,6 @@ class DnD {
     let { folderPaths, filePaths } = args;
     e.stopPropagation();
     this.trashZone.classList.remove("delete__zone--over--dashed");
-    console.log(filePaths);
     const path =
       this.type === "folder"
         ? folderPaths[this.selectedId].path
@@ -72,14 +94,11 @@ class DnD {
 
   async deleteDirectoryApi(path) {
     try {
-      console.log(path);
+      //condition to determine if a file or folder is moved
       if (this.type === "folder")
         await http.delete("/directories/delete", { data: { directory: path } });
       if (this.type === "file")
         await http.delete("/files/delete", { data: { file_dir: path } });
-
-      console.log(this.selectedId);
-      console.log(document.getElementById(this.selectedId));
       unmountComponent(this.selectedId);
       this.trashZone.classList.remove("delete__zone--over");
     } catch (error) {
