d3da4bf34dd8101b6489a1e720675b668336ddd2
Faruq <CodingJay90>
2022-07-16 09:37:48 +0100

    fixes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import swaggerDefinition from "./swagger-definition.js";
 import swaggerJsdoc from "swagger-jsdoc";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //Swagger init
 const swaggerJsdocOptions = {
